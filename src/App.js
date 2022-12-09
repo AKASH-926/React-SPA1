a903fbfa40cc88650dad3c440dbf5721e7e647ed
AKASH <akash.arthanur26@gmail.com>
2022-12-09 14:49:30 +0530

    required folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
     </>
   );
 }
